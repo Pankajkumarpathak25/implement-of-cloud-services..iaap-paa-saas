f88771017e90ccbc7c3ae4b5db6b763348d0748c
pankaj kumar pathak <pankajkumarpathak25@gmail.com>
2018-07-17 14:07:47 +0000

    Add files via upload

---
+++ iaas.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import cgi,commands,cgitb,time,random,os
+
+cgitb.enable()
+print "content-type:text/html"
+print ""
+
+data=cgi.FieldStorage()
+os=data.getvalue('os')
+ram=data.getvalue('ram')
+cpu=data.getvalue('cpu')
+vnc=random.randint(5900,58000)
+web=random.randint(6080,58000)
+
+b=tuple(commands.getoutput('hostname -I').split(' ')[:-1])
+
+off="sudo qrencode -s 16*16 -o {}_{}.png 'http://{}:{}'".format(os,vnc,b[0],web)
+#move="sudo mv /var/www/cgi-bin/None.png /var/www/html/QRCode"
+commands.getoutput(off)
+#commands.getoutput(move)
+
+vm="sudo virt-install --name {} --ram {} --vcpu {} --disk none --cdrom /var/lib/libvirt/images/{}.iso --graphics vnc,listen={},port={} --noautoconsole".format(os,ram,cpu,os,b[0],vnc)
+
+web1="sudo websockify  --web=/usr/share/novnc {} {}:{} -D".format(web,b[0],vnc)
+commands.getoutput(vm)
+#print "vm executed"
+commands.getoutput(web1)
+#print "websockify execeuted"
+#print commands.getoutput('sudo mkdir /root/Desktop/apache')
+#print "<a href='/QRCode/{}_{}.png' target='_blank'>".format(os,vnc)
+
+print "<meta HTTP-EQUIV='refresh' content='0; url=http://{}/pankaj/novnc/vnc_auto.html?ip={}&port={}'/>".format(b[0],b[0],web)
+
